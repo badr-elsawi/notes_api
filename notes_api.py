23aca1326b23fb4bae0fdf8ab8c45b37dd2dd0e9
badr-elsawi <badr.sawy2020@gmail.com>
2024-02-01 16:36:23 -0800

    initiate database

--- notes_api.py
+++ notes_api.py
@@ -1,10 +1,47 @@
 from flask import Flask
-from flask_restful import Resource,abort,Api
+from flask_restful import Api,Resource,abort,reqparse
+from flask_sqlalchemy import SQLAlchemy
 # ***********************************************
-app = Flask(__name__)
-api = Api(app)
+my_app = Flask(__name__)
+api = Api(my_app)
+# initiate database **********
+my_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(my_app)
+class NotesModel(db.Model) :
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.String(500))
+    is_pinned = db.Column(db.Integer)
+    in_trash = db.Column(db.Integer)
 
+with my_app.app_context() :
+    db.create_all()
+# ****************************
 
+# arguments ******************
+add_note_args = reqparse.RequestParser()
+add_note_args.add_argument('title',type=str,help='task is required',required=True)
+add_note_args.add_argument('body',type=str,help='body is required',required=True)
+# *****************************
 
+# request *********************
+class Notes(Resource) :
+    def get(self) :
+        notes = []
+        return {
+            'message' : '',
+            'age' : notes,
+        }
+    
+    def post(self) :
+        note = add_note_args.parse_args()
+        return {
+            'title' : note.title ,
+            'body' : note.body
+        }
+    
+# *****************************
+
+api.add_resource(Notes,'/notes')
 if __name__ == "__main__" :
-    app.run(debug=True,host="0.0.0.0")
+    my_app.run(debug=True,host="0.0.0.0")
