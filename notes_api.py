a819e02f21e41752e823043b5266f49729cd3baf
badr-elsawi <badr.sawy2020@gmail.com>
2024-02-01 17:13:50 -0800

    finish creating and adding notes features

--- notes_api.py
+++ notes_api.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api,Resource,abort,reqparse
+from flask_restful import Api,Resource,abort,reqparse,fields,marshal_with
 from flask_sqlalchemy import SQLAlchemy
 # ***********************************************
 my_app = Flask(__name__)
@@ -22,23 +22,44 @@ with my_app.app_context() :
 add_note_args = reqparse.RequestParser()
 add_note_args.add_argument('title',type=str,help='task is required',required=True)
 add_note_args.add_argument('body',type=str,help='body is required',required=True)
+resource_fields = {
+    'id' : fields.Integer ,
+    'title' : fields.String ,
+    'body' : fields.String ,
+    'is_pinned' : fields.Integer ,
+    'in_trash' : fields.Integer ,
+}
 # *****************************
 
 # request *********************
 class Notes(Resource) :
+    @marshal_with(resource_fields)
     def get(self) :
+        data = NotesModel.query.all()
         notes = []
-        return {
-            'message' : '',
-            'age' : notes,
-        }
-    
+        for note in data :
+            notes.append(
+                {
+                    'id' : note.id ,
+                    'title' : note.title ,
+                    'body' : note.body ,
+                    'is_pinned' : note.is_pinned ,
+                    'in_trash' : note.in_trash ,
+                }
+            )
+        return notes
+    @marshal_with(resource_fields)
     def post(self) :
-        note = add_note_args.parse_args()
-        return {
-            'title' : note.title ,
-            'body' : note.body
-        }
+        args = add_note_args.parse_args()
+        note = NotesModel(
+            title = args['title'] ,
+            body = args['body'] ,
+            is_pinned = 0 ,
+            in_trash = 0 ,
+        )
+        db.session.add(note)
+        db.session.commit()
+        return note , 201
     
 # *****************************
 
