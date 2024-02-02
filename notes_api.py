7e0acbf9d0b169f39b47934bca50b9fb24a1fc8f
badr-elsawi <badr.sawy2020@gmail.com>
2024-02-01 18:16:24 -0800

    finishing update feature

--- notes_api.py
+++ notes_api.py
@@ -20,8 +20,14 @@ with my_app.app_context() :
 
 # arguments ******************
 add_note_args = reqparse.RequestParser()
-add_note_args.add_argument('title',type=str,help='task is required',required=True)
+add_note_args.add_argument('title',type=str,help='title is required',required=True)
 add_note_args.add_argument('body',type=str,help='body is required',required=True)
+put_note_args = reqparse.RequestParser()
+put_note_args.add_argument('id',type=int,help='id is required',required=True)
+put_note_args.add_argument('title',type=str)
+put_note_args.add_argument('body',type=str)
+put_note_args.add_argument('is_pinned',type=int)
+put_note_args.add_argument('in_trash',type=int)
 resource_fields = {
     'id' : fields.Integer ,
     'title' : fields.String ,
@@ -47,6 +53,7 @@ class Notes(Resource) :
                     'in_trash' : note.in_trash ,
                 }
             )
+        
         return notes
     @marshal_with(resource_fields)
     def post(self) :
@@ -60,7 +67,26 @@ class Notes(Resource) :
         db.session.add(note)
         db.session.commit()
         return note , 201
-    
+    @marshal_with(resource_fields)
+    def put(self) :
+        args = put_note_args.parse_args()
+        note = NotesModel.query.filter_by(id = args['id']).first()
+        if not note :
+            abort(404,message='Oooops!, we couldn\'t find this note')
+        if args['title'] :
+            note.title = args['title']
+        if args['body'] :
+            note.body = args['body']
+        if args['is_pinned'] == 0:
+            note.is_pinned = 1
+        if args['is_pinned'] == 1:
+            note.is_pinned = 0
+        if args['in_trash'] == 0:
+            note.in_trash = 1
+        if args['in_trash'] == 1:
+            note.in_trash = 0
+        db.session.commit()
+        return note
 # *****************************
 
 api.add_resource(Notes,'/notes')
